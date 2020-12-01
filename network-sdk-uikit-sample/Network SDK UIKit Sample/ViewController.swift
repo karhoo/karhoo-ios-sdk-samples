1c1293c32a078044f6e787e29abeb4b2b872ca92
EdwardW2 <edward@wilkins-family.org.uk>
2020-12-01 12:05:38 +0000

    no message

---
+++ network-sdk-uikit-sample/Network SDK UIKit Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Network SDK UIKit Sample
+//
+//  Created by Edward Wilkins on 01/12/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
